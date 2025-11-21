cf3fd4b8d491a89b288c21d23b83b0ddb17d4773
Prakash Pandey <prakash.pandey@your-space.in>
2025-11-21 18:25:21 +0530

    fix prisma issue

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,5 +1,5 @@
 import { env } from "~/env";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
