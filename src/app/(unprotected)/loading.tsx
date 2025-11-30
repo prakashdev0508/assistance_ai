a1f29a6a49bd14dbad86cbd465ebffc71430576a
Prakash Pandey <prakash.pandey@your-space.in>
2025-11-30 18:33:47 +0530

    added loader

---
+++ src/app/(unprotected)/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+        <p className='text-sm text-gray-500'>Loading...</p>
+        
+    </div>
+  )
+}
+
+export default loading
